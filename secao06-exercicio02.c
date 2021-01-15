e4665b8f503cd905c9419b74195c4ce3779f702d
Gabriel Dorneles <gabrieldorneles471@gmail.com>
2021-01-15 20:57:54 -0300

    meu primeiro commit

---
+++ secao06-exercicio02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    //Variaveis
+    int numero, a, b;
+
+    //Entradas
+    printf("Informe um numero");
+    scanf("%d", &numero);
+
+    //Processamento
+    if(numero > 0){
+        a = numero;
+        printf("O %d número é positivo.", numero);
+    }else{
+        b = numero;
+        printf("O %d número é negativo.", numero);
+
+    }
+
+
+    }
